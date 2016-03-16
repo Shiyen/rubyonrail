6b0696b2b3607a6ab43e6cad45cad1f6b0a11524
Samson G <SamsonG@Sams-Air.home>
2016-03-15 21:48:23 -0400

    clean up

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ActiveRecord::Base
+	validates :avatar, :attachment_presence => true
+	has_attached_file :avatar, styles: { medium: "640" }
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
